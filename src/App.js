caf390efd7d9c2e82050beed93233af296bcc39d
Amir Behmaram <amirbehmaram4@gmail.com>
2022-01-10 23:38:42 -0800

    Added in some typography stuff, color palette, and setup the basic ciste container with some test content and icon attribution
    Signed-off-by: Amir Behmaram <amirbehmaram4@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import Footer from "./components/footer/footer";
+
+// How to import the SVGs as ReactComponents
+//import { ReactComponent as DragonIcon } from './icons/dragon.svg';
+// Then just use <DragonIcon /> wherever! The ReactComponent part is required.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing out the Github Pages and React setup.
+    <div className="site">
+      <div className="site__content">
+        <h1 className="title">Pokemon Type Helper</h1>
+        <p className="content">
+          This is still a work in progress, but the general idea behind it is to have a simple app to check for the
+          various weaknesses/resistance that any given pokemon may have. I can never keep all that info in my head
+          and it's easier to just use something like this to figure it out on the fly.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.js
@@ -0,0 +1,11 @@
+import "./footer.scss";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__content">Credit to <a href="https://www.sketchappsources.com/contributor/danielmotta">Daniel Motta</a> for the pokemon icons. Thank you!</p>
+    </footer>
+  );
+}
+
+export default Footer;
