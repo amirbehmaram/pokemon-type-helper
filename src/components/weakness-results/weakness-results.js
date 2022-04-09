bab61b048deae139feca3be7cbf4f3caedc94ccc
Amir Behmaram <amirbehmaram4@gmail.com>
2022-04-09 09:59:01 -0700

    Working through the type multiplications
    Signed-off-by: Amir Behmaram <amirbehmaram4@gmail.com>

--- src/components/weakness-results/weakness-results.js
+++ src/components/weakness-results/weakness-results.js
@@ -35,8 +35,8 @@ function WeaknessResults(props) {
     }
   });
 
-  console.log(primaryWeaknesses);
-  console.log(secondaryWeaknesses);
+  // console.log(primaryWeaknesses);
+  // console.log(secondaryWeaknesses);
 
   /**
    * Okay so I think I have the rules for these types pretty clear in my head. I'll try to document it here for future me's sake.
@@ -48,79 +48,8 @@ function WeaknessResults(props) {
    *    example: If a pokemon is dual typed Dragon & Ground it will be 4x weak to Ice since each of those are Double Damaged by Ice.
    *             Another example of this, using that same typing is Grass. A Dual Typed Dragon & Ground pokemon will take normal damage from
    *             a Grass move since Dragon takes 1/2 damage from Grass and Ground takes 2x damage from it.
-   */
-  const setupWeaknesses = () => {
-    // If we don't have a primary then just bail dood
-    if (!primaryWeaknesses) {
-      return;
-    }
-
-    let tempQuadDamage = [],
-        tempDoubleDamage = [],
-        tempNormalDamage = [],
-        tempHalfDamage = [],
-        tempNoDamage = [];
-
-    // To start of easy, setup our no damage values.
-    if (primaryWeaknesses.noDamage.length > 0) {
-      primaryWeaknesses.noDamage.forEach( (weakness) => {
-        pushValueIntoUniqueArray(weakness, tempNoDamage);
-      });
-    }
-
-    if (primaryWeaknesses.halfDamage.length > 0) {
-      primaryWeaknesses.halfDamage.forEach( (weakness) => {
-        pushValueIntoUniqueArray(weakness, tempHalfDamage);
-      });
-    }
-
-    if (primaryWeaknesses.normalDamage.length > 0) {
-      primaryWeaknesses.normalDamage.forEach( (weakness) => {
-        pushValueIntoUniqueArray(weakness, tempNormalDamage);
-      });
-    }
-
-    if (primaryWeaknesses.doubleDamage.length > 0) {
-      primaryWeaknesses.doubleDamage.forEach( (weakness) => {
-        pushValueIntoUniqueArray(weakness, tempDoubleDamage);
-      });
-    }
-
-    // If we have our secondary, we need to do our weakness "multiplications" to get the
-    // actually correct values
-    if (secondaryWeaknesses) {
-
-      // Setup the no damage first since that's the easy one
-      if (secondaryWeaknesses.noDamage.length > 0) {
-        secondaryWeaknesses.noDamage.forEach( (weakness) => {
-          pushValueIntoUniqueArray(weakness, tempNoDamage);
-        });
-      }
-
-      // Check for any quad damages
-      if (secondaryWeaknesses.doubleDamage.length > 0) {
-
-        // Go through our secondary weakness's double damages and see
-        // if it exists in our temp double damage, if it does splice that value
-        // from the temp array, and push it into our quad
-        secondaryWeaknesses.doubleDamage.forEach( (weakness) => {
-          let index = tempDoubleDamage.indexOf(weakness);
-
-          if (index !== -1) {
-            tempDoubleDamage.splice(index, 1);
-            pushValueIntoUniqueArray(weakness, tempQuadDamage);
-          }
-        });
-      }
-
-      // Check for normal x double damage combos
-
-      /****************/
-      /*
-        Left off here - trying to find a smarter/less brute force way to figure out all these type multiplications
-        without makig things so messy. Chart below for an example.
-
-        So we want to take these two:
+   * 
+   * So we want to take these two:
 
         Primary    |  Secondary
         -----------------------
@@ -136,15 +65,94 @@ function WeaknessResults(props) {
         ------------
         1/2  1  2  4
          c   a  d  e
-             b     
-      */
+             b  
+   */
+  const setupWeaknesses = () => {
+    // If we don't have a primary then just bail dood
+    if (!primaryWeaknesses) {
+      return;
+    }
+
+    let tempDoubleDamage = [],
+        tempNormalDamage = [],
+        tempHalfDamage = [],
+        tempNoDamage = [];
+
+    let finalQuadDamage = [],
+        finalDoubleDamage = [],
+        finalNormalDamage = [],
+        finalHalfDamage = [],
+        finalNoDamage = [];
+
+    tempNoDamage = primaryWeaknesses.noDamage;
+    tempHalfDamage = primaryWeaknesses.halfDamage;
+    tempNormalDamage = primaryWeaknesses.normalDamage;
+    tempDoubleDamage = primaryWeaknesses.doubleDamage;
+
+    // If we have our secondary, we need to do our weakness "multiplications" to get the
+    // actually correct values
+    if (secondaryWeaknesses) {
+
+      tempNoDamage = tempNoDamage.concat(secondaryWeaknesses.noDamage);
+      tempHalfDamage = tempHalfDamage.concat(secondaryWeaknesses.halfDamage);
+      tempNormalDamage = tempNormalDamage.concat(secondaryWeaknesses.normalDamage);
+      tempDoubleDamage = tempDoubleDamage.concat(secondaryWeaknesses.doubleDamage);
+
+      // setup quad damage - just take any dups from double and move them into quad
+      tempDoubleDamage.forEach( (weakness) => {
+        let quadValues = [];
+
+        tempDoubleDamage.filter( (val, index) => {
+          if (val === weakness) {
+            quadValues.push(index);
+          }
+        });
+
+        /**
+         * This is stupid looking but let me explain.
+         * 
+         * So if we have multiple values in quad values that means we have some dups that
+         * need to be elevated to quad damage. Since they are they same string I'm just grabbing the first one
+         * from tempDoubleDamage and pushing that string into tempQuad Damage and then I'm going through and 
+         * splicing out both values from tempDoubleDamage using the value to find the index since we are splicing
+         * values out in place.
+         */
+        if (quadValues.length > 1) {
+          
+          let quadDamageName = tempDoubleDamage[quadValues[0]];
+          finalQuadDamage.push(quadDamageName);
+          
+          for(let index = 0; index < quadValues.length; index++) {
+            let spliceIndex = tempDoubleDamage.indexOf(quadDamageName);
+            tempDoubleDamage.splice(spliceIndex, 1);
+          }
+        }
+      });
+
+      // Check Double vs normal
+      sortWeaknesses(tempDoubleDamage, tempNormalDamage, finalDoubleDamage);
+
+      // Check Double vs 1/2
+      sortWeaknesses(tempDoubleDamage, tempHalfDamage, finalNormalDamage);
+
+      // Check Double vs no
+      sortWeaknesses(tempDoubleDamage, tempNoDamage, finalNoDamage);
+
+      // Check Normal vs 1/2
+      sortWeaknesses(tempNormalDamage, tempHalfDamage, finalHalfDamage);
+
+      // Check Normal vs no
+      sortWeaknesses(tempNoDamage, tempNoDamage, finalNoDamage);
+
+      // Check 1/2 vs no
+      sortWeaknesses(tempHalfDamage, tempNoDamage, finalNoDamage);
     }
     
-    console.log(tempNoDamage);
-    console.log(tempHalfDamage);
-    console.log(tempNormalDamage);
-    console.log(tempDoubleDamage);
-    console.log(tempQuadDamage);
+    console.log(finalNoDamage);
+    console.log(finalHalfDamage);
+    console.log(finalNormalDamage);
+    console.log(finalDoubleDamage);
+    console.log(finalQuadDamage);
   };
 
   
@@ -189,5 +197,21 @@ export default WeaknessResults;
 function pushValueIntoUniqueArray(value, arr) {
   if (arr.indexOf(value) === -1) {
     arr.push(value);
+  }
+}
+
+function sortWeaknesses(weaknessArrOne, weaknessArrTwo, finalDestination) {
+
+  let weaknessTwoCopy = weaknessArrTwo;
+
+  for(let indexOne = 0; indexOne < weaknessArrOne.length; indexOne++) {
+
+    for(let indexTwo = 0; indexTwo < weaknessTwoCopy.length; indexTwo++) {
+      
+      if (weaknessTwoCopy[indexTwo] === weaknessArrOne[indexOne]) {
+        pushValueIntoUniqueArray(weaknessTwoCopy[indexTwo], finalDestination);
+        weaknessTwoCopy.splice(indexTwo, 1);
+      }
+    }
   }
 }
